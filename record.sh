f4e3f7b40f1efcf2dbb51942320e5e7136ed54c0
Chris Grimmett <chris@grimtech.net>
2015-04-25 04:26:22 -0700

    making 1h recording segments

--- record.sh
+++ record.sh
@@ -1,2 +1,11 @@
-ffmpeg -video_size 1366x768 -framerate 2 -f x11grab -i :0.0 ./out1.mp4
+#ffmpeg -video_size 1366x768 -framerate 12 -f x11grab -i :0.0 ./out1.mp4
+
+duration=3
+stamp=0
+while [ 0 -eq 0 ]; do
+    echo "recording video $stamp for $duration seconds"
+    ffmpeg -t $duration -video_size 1366x768 -framerate 12 -f x11grab -i :0.0 ./vids/out_$stamp.mp4
+    sleep 1
+    let stamp++
+done
 
