17c0a3c0f423bde09f2f3e0f466db92fcd52387b
Chris Grimmett <chris@grimtech.net>
2015-04-26 10:43:53 -0700

    change 3s to 3600

--- record.sh
+++ record.sh
@@ -1,11 +1,11 @@
 #ffmpeg -video_size 1366x768 -framerate 12 -f x11grab -i :0.0 ./out1.mp4
 
-duration=3
+duration=3600
 stamp=0
 while [ 0 -eq 0 ]; do
     echo "recording video $stamp for $duration seconds"
-    ffmpeg -t $duration -video_size 1366x768 -framerate 12 -f x11grab -i :0.0 ./vids/out_$stamp.mp4
     sleep 1
+    ffmpeg -t $duration -video_size 1366x768 -framerate 12 -f x11grab -i :0.0 ./vids/out_$stamp.mp4
     let stamp++
 done
 
